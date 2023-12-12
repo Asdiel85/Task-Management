29efb3cb17ce6fc863f5df9c242faf3d05ef1dc1
Martin Hristov <mghristov85@gmail.com>
2023-12-12 10:40:02 +0200

    added user login

--- server/src/managers/userManager.js
+++ server/src/managers/userManager.js
@@ -2,3 +2,40 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("../lib/jwt");
 const { SECRET } = require("../config/config");
+
+exports.register = async (userData) => {
+    const user = await User.findOne({email: userData.email})
+
+    if(!user) {
+        await User.create(userData)
+    } else {
+        throw new Error('User already exists')
+    }
+
+    exports.login = async (email, password) => {
+        const user = await User.findOne({email})
+
+        if(!user) {
+            throw new Error('Invalid email or password')
+        }
+        const isValid = await bcrypt.compare(password, user.password)
+       
+        if(!isValid) {
+            throw new Error('Invalid email or password')
+        }
+
+        const payload = {
+            id: user._id,
+            username: user.username,
+          };
+
+          const userData = {
+            id: user._id,
+            username: user.username,
+            isAdmin: user.isAdmin
+          }
+          userData.token = await jwt.sign(payload, SECRET, { expiresIn: '2d' });
+
+          return userData;
+    }
+}
