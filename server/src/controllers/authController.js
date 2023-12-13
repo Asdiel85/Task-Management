21d7b8766467fde463298cba2bb42a8eee90e1de
Martin Hristov <mghristov85@gmail.com>
2023-12-13 12:26:14 +0200

    added register and login

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const userManager = require('../managers/userManager');
+
+router.post('/register', async (req, res) => {
+    const {firstName,lastName, username, email, password, repeatPassword} = req.body;
+
+    try {
+        await userManager.register({firstName,lastName, username, email, password, repeatPassword})
+        res.status(200).json('User created')
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const token = await userManager.login(email, password);
+      res.status(200).json(token);
+    } catch (error) {
+      res.status(400).json(error.message);
+    }
+  });
+  
+  module.exports = router;
