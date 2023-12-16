8aa43d56ea89475d134f454244de36807864c49c
Martin Hristov <mghristov85@gmail.com>
2023-12-16 08:32:22 +0200

    added constants file

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = 'http://localhost:3000/';
+export const CREATE = '/create'
+
+export const AUTH = 'auth';
+export const REGISTER = '/register'
+export const LOGIN = '/login'
+
+export const TASKS = 'tasks'
+
+export const USERS = 'users'
