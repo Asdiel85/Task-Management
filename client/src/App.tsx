a7155b3ce9a0a1e67a15b49224f1855c25217806
Martin Hristov <mghristov85@gmail.com>
2023-12-16 10:08:22 +0200

    added bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import styles from './App.module.css';
 import { AuthProvider } from "./context/AuthContext"
+import { Register } from './components/Register/Register';
+import { Login } from './components/Login/Login';
+import { Navigation } from './components/Navigation/Navigation';
 
 
 export const App =() => {
 
   return (
     <AuthProvider>
-    <h1>Hello From App</h1>
+      <Navigation />
+      <main className={styles.container}>
+    <Routes>
+    <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />
+    </Routes>
+    </main>
     </AuthProvider>
   )
 }
