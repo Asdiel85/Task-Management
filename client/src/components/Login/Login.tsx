f4cb9d051e4145e3b637f7897661b94112f1ef35
Martin Hristov <mghristov85@gmail.com>
2023-12-18 10:55:56 +0200

    login works but needs refactor

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,18 +1,39 @@
 import Form from "react-bootstrap/Form";
 import styles from './Login.module.css'
-import { FC } from "react";
+import * as authService from '../../service/authService'
+import { ChangeEvent, FC, useState } from "react";
+import { handleResponse } from "../../utils/handleResponse";
+import { loggedInUser } from "../../utils/types";
 
 export const Login: FC = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
+    } else {
+      setPassword(event.target.value);
+    } 
+  }
+
+  const handleSubmit = async (event: React.SyntheticEvent): Promise<void> => {
+    event.preventDefault();
+   const response: Response = await authService.login({email, password});
+   const userData: loggedInUser = await handleResponse(response);
+   console.log(userData);
+   
+  }
   return (
     <div className={styles.formHolder}>
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="email">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control name="email" type="email" placeholder="name@example.com" onChange={handleChange} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="password" />
+        <Form.Control name="password" type="password" placeholder="password" onChange={handleChange} />
       </Form.Group>
       <input type="submit" value= 'Submit' />
     </Form>
