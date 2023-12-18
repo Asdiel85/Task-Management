6714f543bf6169e56c65e1d457a960fbed1c4544
Martin Hristov <mghristov85@gmail.com>
2023-12-18 10:23:37 +0200

    added login form

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,5 +1,21 @@
+import Form from "react-bootstrap/Form";
+import styles from './Login.module.css'
 import { FC } from "react";
 
 export const Login: FC = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className={styles.formHolder}>
+    <Form>
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="password" />
+      </Form.Group>
+      <input type="submit" value= 'Submit' />
+    </Form>
+    </div>
+  );
 };
