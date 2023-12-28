e3410f423f855c7581487a94772d1af3321830fb
Martin Hristov <mghristov85@gmail.com>
2023-12-28 12:01:27 +0200

    there is a bug when editining task name, it is removing the status

--- client/src/components/TasksTable/TasksTable.tsx
+++ client/src/components/TasksTable/TasksTable.tsx
@@ -1,17 +1,33 @@
 import Table from "react-bootstrap/Table";
-import { ChangeEvent, FC, FormEvent, SyntheticEvent, useContext, useEffect, useState } from "react";
+import {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  SyntheticEvent,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import * as taskService from "../../service/taskService";
 import { UserContext } from "../../context/AuthContext";
 import { IAuthContext, Task } from "../../utils/types";
 
 export const TasksTable: FC = () => {
+  const initialTask: Task = {
+    _id: '',
+    name: '',
+    status: 'Not Started',
+    owner: '',
+    todos: [],
+  };
+
   const { loggedUser } = useContext<IAuthContext>(UserContext);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [status, setStatus] = useState<string>("");
-  const [taskId, setTaskId] = useState<string>("");
+  const [taskId, setTaskId] = useState<string>(initialTask._id);
   const [showEditTaskInput, setShowEditTaskInput] = useState<boolean>(false);
-  const [taskName, setTaskName] = useState<string>("");
-  const [task, setTask] = useState<Task | null>(null)
+  const [taskName, setTaskName] = useState<string>(initialTask.name);
+  const [task, setTask] = useState<Task>(initialTask);
 
   useEffect(() => {
     taskService.getAllTasks(loggedUser?.id!).then((data) => {
@@ -23,26 +39,35 @@ export const TasksTable: FC = () => {
     event: ChangeEvent<HTMLSelectElement>,
     taskId: string
   ): void => {
-    setStatus(event.currentTarget.value);
+    setStatus(event.target.value);
     setTaskId(taskId);
   };
 
-  const handleTaskNameChange = (
-    event: FormEvent<HTMLInputElement>,
-  ): void => {
-    setTaskName(event.currentTarget.value);
+  const handleTaskNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask((prevTask) => {
+      return {
+        ...prevTask,
+        name: e.target.value,
+      };
+    });
+    console.log(taskName);  
   };
 
   const hadnleEditButtonClick = (task: Task): void => {
-    setShowEditTaskInput(true)
-    setTask(task)
-  }
+    setShowEditTaskInput(true);
+    setTask(task);
+    setTaskId(task._id)
+  };
 
-  const handleApplyButtonChange = async (event: SyntheticEvent, taskName: string, taskId: string): Promise<void> => {
+  const handleApplyButtonChange = async (
+    event: SyntheticEvent,
+    taskName: string,
+    taskId: string
+  ): Promise<void> => {
     event.preventDefault();
-    const newTask = await taskService.editTask(taskId, taskName)
-    setShowEditTaskInput(false)
-  }
+    const newTask = await taskService.editTask(taskId, taskName);
+    setShowEditTaskInput(false);
+  };
   useEffect(() => {
     if (taskId !== "") {
       taskService.updateTaskStatus(taskId, status).then((res) => res.json());
@@ -50,44 +75,59 @@ export const TasksTable: FC = () => {
   }, [taskId]);
 
   return (
-    <>    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Task Name</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasks.map((task: Task) => (
-          <tr key={task._id}>
-              <td>{task.name}</td>
-            <td>
-              <select
-                onChange={(event) => handleStatusChange(event, task._id)}
-                name="status"
-                id="status"
-                defaultValue={task.status}
-              >
-                <option value="Not Started">Not Started</option>
-                <option value="Pending">Pending</option>
-                <option value="Finnished">Finnished</option>
-              </select>
-            </td>
-            <td><button onClick={() =>hadnleEditButtonClick(task)}>Edit</button></td>
+    <>
+      {" "}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Task Name</th>
+            <th>Status</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
-    {showEditTaskInput && (
-      <>
-              <input
-                name="editTask"
-                value={task?.name}
-                onChange={(event) => handleTaskNameChange(event)}
-              />
-              <button onClick={(event) => handleApplyButtonChange(event, taskName, task?._id!)}>Apply</button>
-              </>
-            )}
+        </thead>
+        <tbody>
+          {tasks.map((task: Task) => (
+            <tr key={task._id}>
+              <td>{task.name}</td>
+              <td>
+                <select
+                  onChange={(event) => handleStatusChange(event, task._id)}
+                  name="status"
+                  id="status"
+                  defaultValue={task.status}
+                >
+                  <option value="Not Started">Not Started</option>
+                  <option value="Pending">Pending</option>
+                  <option value="Finnished">Finnished</option>
+                </select>
+              </td>
+              <td>
+                <button onClick={() => hadnleEditButtonClick(task)}>
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {showEditTaskInput && (
+        <>
+          <label htmlFor="editTask">Task Name</label>
+          <input
+            type="text"
+            name="editTask"
+            id="editTask"
+            onChange={handleTaskNameChange}
+            value={task.name}
+          />
+          <button
+            onClick={(event) =>
+              handleApplyButtonChange(event, task.name, taskId)
+            }
+          >
+            Apply
+          </button>
+        </>
+      )}
     </>
   );
 };
